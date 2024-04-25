9e271d32fbd2fcf84370134f4b1c5b6a36c89991
Mradulgupta1 <161187460+Mradulgupta1@users.noreply.github.com>
2024-04-25 23:12:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        blue: "#3751FE",
+        grey: "#E5E5E5",
+      },
+    },
+  },
+  plugins: [],
+}
+
